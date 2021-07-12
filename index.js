b3fb3b5113ae3d6bd804852a5524779cec56d8a2
Vitaly <kaliummati@gmail.com>
2021-07-12 09:51:17 +0900

    update

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ import { START_ACTION_NAME } from './core/start/index.js';
 import { saveMessageIdInSessionFromQueryHelper } from './main/telegram/index.js';
 import { startSchedule } from './main/scheduler/index.js';
 
-import { testSchedule } from './schedule.js';
-
 const start = () => {
   try {
     bot.command(START_ACTION_NAME, ctx => { 
@@ -16,10 +14,5 @@ const start = () => {
     setTimeout(() => start(), 20000);
   }
 };
-
-// setTimeout(() => {
-//   console.log('test start');
-//   testSchedule();
-// }, 40000);
 
 start();
